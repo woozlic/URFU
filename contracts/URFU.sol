f985889dd7d59bff81c29161e527c27ca2ae761f
woozlic <sampaccs@mail.ru>
2023-05-21 19:44:25 +0500

    up

--- contracts/URFU.sol
+++ contracts/URFU.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+contract URFU is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("URFU Token", "URFU") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
